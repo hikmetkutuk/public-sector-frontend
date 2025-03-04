88f0fc4e8d5fd0b915e5366e9fb7b9b9f94df706
hikmetkutuk <hikmetkutuk@gmail.com>
2025-03-04 11:24:24 +0300

    feat: add organization page and update menu icons with consistent styling

---
+++ src/app/organisation/page.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb';
+import DefaultLayout from '@/components/Layouts/DefaultLayout';
+
+const Page = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Organizasyon Şeması" />
+    </DefaultLayout>
+  );
+};
+
+export default Page;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,20 +1,29 @@
-import { House, FileSliders, Variable } from 'lucide-react';
+import { House, FileSliders, Variable, FolderTree } from 'lucide-react';
 import React from 'react';
 
 export const menuGroups = [
   {
     menuItems: [
       {
-        icon: React.createElement(House),
+        icon: React.createElement(House, { className: 'menu-icon' }),
         label: 'Ana Sayfa',
         route: '/',
       },
       {
-        icon: React.createElement(FileSliders),
+        icon: React.createElement(FileSliders, { className: 'menu-icon' }),
         label: 'Sistem Parametreleri',
         route: '#',
         children: [
-          { label: 'Tanımlar', route: '/definition', icon: React.createElement(Variable) },
+          {
+            label: 'Tanımlar',
+            route: '/definition',
+            icon: React.createElement(Variable, { className: 'menu-icon' }),
+          },
+          {
+            label: 'Organizasyon',
+            route: '/organisation',
+            icon: React.createElement(FolderTree, { className: 'menu-icon' }),
+          },
         ],
       },
     ],
